864a6b93cd0e336c0aaf9df05bc0b23a583f794b
于景洋 <lovesyxfuffy@gmail.com>
2017-10-10 19:30:21 +0800

    add Userutil

---
+++ src/main/java/com/driverPlus/dao/po/UserInfo.java
@@ -0,0 +1,14 @@
+package com.driverPlus.dao.po;
+
+import lombok.Data;
+
+/**
+ * Created by yujingyang on 2017/10/10.
+ */
+@Data
+public class UserInfo {
+    private Integer accountId;
+    private Integer schoolId;
+    private Integer roleId;
+
+}
---
+++ src/main/java/com/driverPlus/utils/UserUtil.java
@@ -0,0 +1,31 @@
+package com.driverPlus.utils;
+
+import com.driverPlus.dao.po.UserInfo;
+
+/**
+ * Created by yujingyang on 2017/10/10.
+ */
+public class UserUtil {
+
+    public static UserInfo userInfo;
+
+    static {
+        userInfo = new UserInfo();
+        userInfo.setSchoolId(1);
+        userInfo.setRoleId(0);
+        userInfo.setAccountId(0);
+    }
+
+    public static Integer getSchoolId(){
+        return userInfo.getSchoolId();
+    }
+
+    public static Integer getAccountId(){
+        return userInfo.getAccountId();
+    }
+
+    public static UserInfo getUserInfo(){
+        return userInfo;
+    }
+
+}
