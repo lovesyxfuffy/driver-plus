ad485e738578d0b60b25210c7c71a3ff0054f0d8
于景洋 <lovesyxfuffy@gmail.com>
2017-10-16 19:17:38 +0800

    update mapper

---
+++ src/main/java/com/driverPlus/dao/po/manage/Answer.java
@@ -0,0 +1,75 @@
+package com.driverPlus.dao.po.manage;
+
+import java.util.Date;
+
+public class Answer {
+    private Integer id;
+
+    private Integer questionId;
+
+    private Boolean right;
+
+    private String content;
+
+    private String number;
+
+    private Integer status;
+
+    private Date addTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Boolean getRight() {
+        return right;
+    }
+
+    public void setRight(Boolean right) {
+        this.right = right;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number == null ? null : number.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+}
